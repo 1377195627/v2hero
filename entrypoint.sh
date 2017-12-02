7725c783724e435295fce04457aaedf3fda8a2f8
1377195627 <1377195627@qq.com>
2017-12-02 19:44:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /v2raybin
 echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
+echo -e -n "$PORTS" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
